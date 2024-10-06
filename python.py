4e3126ac7dc867f8a4ab6b242c86199af6da7f08
Andreola <pedroandreola05@gmail.com>
2024-10-06 04:33:24 -0300

    Adição da contagem e porcentagem de clientes e cancelamentos

--- python.py
+++ python.py
@@ -1,7 +1,23 @@
+# Importar Bibliotecas
 from IPython.display import display
 import pandas as pd
+
+# Configuração do Display
 pd.set_option('display.max_columns', None)
+
+# Importar base de dados
 tabela = pd.read_csv('./ClientesBanco.csv', encoding="latin1")
+
+# Remover a coluna ClienteNum
 tabela = tabela.drop("CLIENTNUM", axis=1)
+
+# Remover linhas que tenham um valor vazio
 tabela = tabela.dropna()
-display(tabela.describe().round(1))
+
+# Exibe o numero de Clientes x Cancelamentos
+qtde_categoria = tabela["Categoria"].value_counts()
+display(qtde_categoria)
+
+# Exibe a porcentagem de Clientes x Cancelamentos
+qtde_categoria_perc = tabela["Categoria"].value_counts(normalize=True)
+display(qtde_categoria_perc)
